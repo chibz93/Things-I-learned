511a6fec5cc91e388194c58f51bb0a8eb14ceb77
Chibuzo Nwobiri <chibuzo.nwobiri@qa.com>
2017-04-05 11:22:51 +0100

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * Created by Chibuzo Nwobiri on 04/04/2017. Here, I'm trying to chain constructors
+ */
+public class Teacher {
+    private String teacher;
+    private String lesson;
+    private int classNo;
+
+    public Teacher(String teacher, String lesson){
+        this.classNo = classNo;
+        this.teacher = teacher;
+        this.lesson = lesson;
+    }
+
+    public Teacher(String teacher, int classNo){
+        this(classNo);
+    }
+
+    public Teacher(int classNo) {
+    }
+
+
+}
