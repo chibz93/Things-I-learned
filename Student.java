6058f492ff20aa64afe64907d38aa4dff34dc302
Chibuzo Nwobiri <chibuzo.nwobiri@qa.com>
2017-05-24 17:28:14 +0100

    Add files via upload

--- Student.java
+++ Student.java
@@ -4,6 +4,19 @@ package com.company;
  * Created by Chibuzo Nwobiri on 11/05/2017.
  */
 public class Student extends School{
-    private int year;
-    private String subject;
+    int[] year = new int[]{7,8,9,10,11};
+    String[] Subject = new String[]{"English", "Maths", "Science", "History", "IT", "French", "Spanish"};
+    String[] Grade = new String[]{"A","B","C","D","E"};
+
+    public Student(){
+
+    }
+
+    public static int[][] timeTable(){
+
+    }
+
+    public String[][] getGrades(){
+        
+    }
 }
